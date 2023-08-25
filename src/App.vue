c57bef1674db67ed8ef00285b532c922425e343c
Sergio Castillo <shcastillosegura@gmail.com>
2023-08-25 14:02:42 -0500

    imageapp terminado

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <v-app>
     <!-- sidebar -->
+    <Sidebar @selectOption="onSelectOption"></Sidebar>
     <v-main>
       <!-- cards -->
-      <Sidebar></Sidebar>
+      <ImageGallery :isWithColor="isWithColor"></ImageGallery>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import Sidebar from './components/Sidebar.vue';
+import ImageGallery from './components/ImageGallery.vue';
+import { ref } from 'vue';
+
+const isWithColor = ref(true);
+const onSelectOption =(isColor)=>{
+  isWithColor.value=isColor
+}
 </script>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,6 +3,43 @@
         expand-on-hover
         rail
       >
-      asdas
+      <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-image" title="Color Gallery" value="color"
+          @click="selectOption(true)"
+          >
+          </v-list-item>
+
+          <v-list-item prepend-icon="mdi-dialpad" title="Space Gallery" value="gallery"
+          @click="selectOption(false)"
+          >
+
+          </v-list-item>
+
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred">
+    
+          </v-list-item>
+        </v-list>
       </v-navigation-drawer>
 </template>
+
+<script setup>
+
+import { defineEmits } from 'vue';
+
+const emits = defineEmits(['selectOption'])
+
+const selectOption = (isColor)=>{
+    emits("selectOption",isColor)
+}
+
+</script>
